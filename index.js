d6317f5af72a3789b27cfeb3a4c92c45ae34f4c9
Irene <irenemanningm@gmail.com>
2021-11-10 15:12:58 -0600

    commit tree 189818c2994cd8bb059bfd6fdd0d38e76cde1ed6

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solution here
+function findMatching(array, string) {
+    return array.filter(function(el) {
+      return el.toLowerCase().indexOf(string.toLowerCase()) !== -1
+    })
+  }       
+
+  function fuzzyMatch(array, string) {
+      return array.filter(function(element) {
+          return element.toLowerCase().indexOf(string.toLowerCase()) === 0
+        })
+    }    
+
+function matchName(array, string) {
+    return array.filter(function(element){
+        return element.name === string
+    })
+}
